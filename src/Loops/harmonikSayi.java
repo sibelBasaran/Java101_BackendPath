b758c44004540c73e96a1d2929e02c404f1e7ae1
SIBEL BASARAN <44551547+sibelBasaran@users.noreply.github.com>
2023-07-23 14:18:25 +0300

    Add files via upload

---
+++ src/Loops/harmonikSayi.java
@@ -0,0 +1,17 @@
+package Loops;
+import java.util.Scanner;
+
+public class harmonikSayi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("N sayısını giriniz: ");
+        int n = input.nextInt();
+        double result = 0.0;
+        for( double i=1; i <=n; i++){
+             result += (1/i);
+
+        }
+        System.out.println(result);
+
+    }
+}
