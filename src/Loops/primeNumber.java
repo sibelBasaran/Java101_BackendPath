84e912a4e35c0f43e3f606f0245255e347316893
SIBEL BASARAN <44551547+sibelBasaran@users.noreply.github.com>
2023-09-01 22:38:45 +0300

    Add files via upload

---
+++ src/Loops/primeNumber.java
@@ -0,0 +1,22 @@
+package Loops;
+
+public class primeNumber {
+    public static void main(String[] args) {
+        System.out.println("1 - 100 arasındaki Asal Sayılar:");
+
+        for (int sayi = 2; sayi <= 100; sayi++) {
+            boolean asalMi = true;
+
+            for (int i = 2; i <= sayi/2; i++) {
+                if (sayi % i == 0) {
+                    asalMi = false;
+                    break;
+                }
+            }
+
+            if (asalMi) {
+                System.out.print(sayi + " ");
+            }
+        }
+    }
+}
