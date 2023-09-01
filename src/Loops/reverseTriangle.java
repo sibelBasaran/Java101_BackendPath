38d61053d9720398be2dc2b4fa16f1c2024d791b
SIBEL BASARAN <44551547+sibelBasaran@users.noreply.github.com>
2023-09-01 22:14:41 +0300

    Add files via upload

---
+++ src/Loops/reverseTriangle.java
@@ -0,0 +1,21 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class reverseTriangle {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("Basamak sayısını giriniz: ");
+        int basamak = input.nextInt();
+
+        for(int i=basamak; i>=1;i--){
+            for(int j=1;j<=basamak-i;j++){
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
