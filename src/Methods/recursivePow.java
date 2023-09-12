a0b2533bd30751ca0f5e9be756bbc367f399928b
sibelBasaran <basaransibel01.com>
2023-09-12 10:17:10 +0300

    adding files

---
+++ src/Methods/recursivePow.java
@@ -0,0 +1,25 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class recursivePow {
+        static int usAl(int taban, int us) {
+            if (us == 0)
+                return 1;
+            else
+                return taban * usAl(taban, us - 1);
+        }
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Taban değerini giriniz: ");
+            int taban = scanner.nextInt();
+
+            System.out.print("Üs değerini giriniz: ");
+            int us = scanner.nextInt();
+
+            int sonuc = usAl(taban, us);
+            System.out.println("Sonuç: " + sonuc);
+    }
+}
